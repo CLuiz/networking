266ac6ac2e79dcda65724d24d08e0dee8563e390
CLuiz <chris@chrisluiz.io>
2016-08-23 14:19:08 -0400

    tcp proxy work

--- tcp_proxy.py
+++ tcp_proxy.py
@@ -56,6 +56,82 @@ def proxy_handler(client_socket, remote_host, remote_port, receive_first):
         #rinse, wash, repeat
         while True:
             
+            #read from local host
+            local_buffer_ = receive_from(client_socket)
+            
+            if len(local_buffer):
+                print "[==>] Received %d bytes from localhost." % len(local_buffer)
+                hexdump(local_buffer)
+                
+                #send it to our request handler
+                local_buffer = request_handler(local_buffer)
+                
+                #send off the data to the remote host
+                remote_socket.send(local_buffer)
+                print "[==>] Sent to remote"
+                
+            #receive back the response
+            remote_buffer = receive_from(remote_socket)
+            
+            if len(remote_buffer):
+                print "[<==] Received %d bytes from remote." % len(remote_buffer)
+                hexdump(remote_buffer)
+                
+                #send to our reponse handler
+                remote_buffer = response_handler(remote_buffer)
+                
+                #send the response to the local socket
+                client_socket.send(remote_buffer)
+                
+                print "[<==] Sent to localhost"
+                
+            # if no more data on either side close the connections
+            if not len(local_buffer) or not len(remote_buffer):
+                client_socket.close()
+                remote_socket.close()
+                print "[*] No more data. Closing connections."
+                
+                break
+   
+           
+def hexdump(src, length=16):
+    #this hexdump function is taken directly from:
+    # http://code.activestate.com/recipes/142812-hex-dumper/
+    
+    results = []
+    digits = 4 if isinstance(src, unicode) else 2
+    for i in xrange(0, len(src), length):
+        s = src[i:i+length]
+        hexa = b' '.join(["%0*X" % (digits, ord(x)) for x in s])
+        text = b''.join([x if 0x20 <= ord(x) < 0x7F else b'.' for x in s])
+        result.append(b"%04X %-*s %s" % (i, lenght*(digits+1), hexa, text))
+        
+    print b'\n'.join(result)
+    
+def receive_from(connection):
+    
+    buffer = ""
+    
+    #we set a 2 second timeout, 
+    #depending on the target, this may need to be adjusted
+    connection.settimeout(2)
+    
+    try:
+        
+        #keep reading into the buffer until there
+        #is no more data, or we run out of time
+        while True:
+            data = connection.recv(4096)
+            
+            if not data:
+                break
+            buffer += data
+    except:
+        pass
+    
+    return buffer
+
+        
         
     
 def main():
