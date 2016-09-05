5ea3d6bb144d54fc82cf24e61c8dad9ec6f03fb9
CLuiz <chris@chrisluiz.io>
2016-09-05 11:07:53 -0400

    sniffer work

--- sniffer_ip_header_decode.py
+++ sniffer_ip_header_decode.py
@@ -9,7 +9,7 @@ host = '192.168.0.187'
 #our IP header
 
 class IP(Structure):
-    _fields = [
+    _fields_ = [
         ("ihl",          c_ubyte,4),
         ("version"       c_ubyte,4),
         ("tos"           c_ubyte),
@@ -32,4 +32,47 @@ class IP(Structure):
         self.protocol_map = {1:"ICMP", 6:"TCP", 17: "UDP"}
         
         #human readable IP addresses
+        self.src_address = socket.inet_ntoa(struct.pack("L",self.src))
+        self.dst_address = socket.inet_ntoa(struct.pack("L",self.dst))
         
+        #human readable protocol
+        try:
+            self.protocol = self.protocol_map[self.protocol_num]
+        except:
+            self.protocol = str(self.protocol_num)
+            
+#similar to previous script examples
+
+if os.name == 'nt':
+    socket_protocol = socket.IPPROTO_IP
+else:
+    socket_protocol = socket.IPPROTO_ICMP
+    
+sniffer = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket_protocol)
+
+sniffer.bind((host, 0))
+sniffer.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
+
+if os.name == 'nt':
+    sniffer.iotcl(socket.SIO_RCVALL, socket.RCVALL_ON)
+    
+try:
+    while True:
+        
+        #read in a packet
+        raw_buffer = sniffer.recvfrom(65565)[0]
+        
+        #create an IP header fromt he first 20 bytes of the buffer
+        ip_header = IP(raw_buffer[0:20])
+        
+        #print out the protocol that was detected and the hosts
+        print 'Protocol: %s %s -> %s' % (ip_header.protocol, 
+                                         ip_header.src_address,
+                                         ip_header.dst_address)
+    
+    #handle CTRL-C
+    except KeyboardInterrupt:
+        #if using windows turn off promiscuous mode
+        if os.name == 'nt':
+            sniffer.iotcl(socket.SIO_RCVALL, socket.RCVALL_OFF)
+            
