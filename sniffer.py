92e0cb502e626e7c3ce1fe6af87f8146dee43070
Chris Luiz <chris@chrisluiz.io>
2017-09-26 06:05:39 -0400

    formatting

--- sniffer.py
+++ sniffer.py
@@ -1,31 +1,34 @@
 import socket
 import os
 
-#host to listen on
-host = '192.168.0.196'
 
-#create a raw socket and bind it to the public interface
-if os.name == 'nt':
-    socket_protocol = socket.IPPROTO_IP
-else:
-    socket_protocol = socket.IPPROTO_ICMP
-    
-sniffer = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket_protocol)
+def main():
+    # host to listen on
+    host = '192.168.0.196'
 
-sniffer.bind((host, 50007))
+    # create a raw socket and bind it to the public interface
+    if os.name == 'nt':
+        socket_protocol = socket.IPPROTO_IP
+    else:
+        socket_protocol = socket.IPPROTO_ICMP
 
-#we want the IP headers included in the capture
-sniffer.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
+    sniffer = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket_protocol)
+    sniffer.bind((host, 50007))
+    # we want the IP headers included in the capture
+    sniffer.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
 
-#if we are using windows we need to send an IOCTL
-#to set up promiscuous mode
-if os.name =='nt':
-    sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
-    
-#read in a single packet
-print sniffer.recvfrom(65565)
+    # if we are using windows we need to send an IOCTL
+    # to set up promiscuous mode
+    if os.name == 'nt':
+        sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
 
-#if we a re using windows, turn off promiscuous mode
-if os.name == 'nt':
-    sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_OFF)
-    
+    # read in a single packet
+    print(sniffer.recvfrom(65565))
+
+    # if we a re using windows, turn off promiscuous mode
+    if os.name == 'nt':
+        sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_OFF)
+
+
+if __name__ == '__main__':
+    main()
